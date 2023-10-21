97194e874172900d149dcb06cb8ec925ef81c856
AnnaZubakin <146950990+AnnaZubakin@users.noreply.github.com>
2023-10-21 17:09:40 +0300

    AutoLayout finally
    October 21

--- AutoLayout/Controller/ViewController.swift
+++ AutoLayout/Controller/ViewController.swift
@@ -21,6 +21,39 @@ class ViewController: UIViewController {
         }
     }
 
-
+    @IBAction func buttonC(_ sender: UIButton) {
+        sender.backgroundColor = UIColor.red
+    }
+    
+    
+    @IBAction func buttonD(_ sender: UIButton) {
+        view.backgroundColor = UIColor.green
+    }
+  
+    
+    @IBAction func ButtonE(_ sender: UIButton) {
+        sender.setTitle("New name", for: .normal)
+    }
+    
+    
+    @IBAction func ButtonA(_ sender: UIButton) {
+        sender.isHidden = true
+        changeColor(of: styleOutletCollection[2], to: .systemIndigo)
+        changeColor(of: styleOutletCollection[2], to: .systemIndigo)
+        changeColor(of: styleOutletCollection[2], to: .systemIndigo)
+    }
+ 
+    
+    @IBAction func ButtonB(_ sender: UIButton) {
+        changeColor(of: styleOutletCollection[2], to: .systemIndigo)
+        view.backgroundColor = UIColor.white
+        styleOutletCollection[0].isHidden = false
+        styleOutletCollection[4].setTitle("Change name", for: .normal)
+    }
+    
+    func changeColor(of button: UIButton, to color: UIColor) {
+        button.backgroundColor = color
+    }
+    
 }
 
